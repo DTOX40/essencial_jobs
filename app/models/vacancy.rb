1d208e71b320aace6103b7dccb35e47580f68693
Denis abreu30 <denis3.0abreu@gmail.com>
2022-02-24 16:14:23 -0300

    validação de vaga

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,6 @@
 class Vacancy < ApplicationRecord
   belongs_to :company
   has_many :applicants
+  validates_presence_of :title, message: ' não pode ficar em branco'
+  validates_presence_of :location, message: 'não pode ficar em branco'
 end
